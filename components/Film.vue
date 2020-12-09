ec0a31cf63a08dcab1905dfead572303f1c797ee
gdarquie <gaetan.darquie@gmail.com>
2020-12-10 00:09:06 +0100

    Initial commit

---
+++ components/Film.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <article class="film">
+      <h2>{{film.title[0].value}}</h2>
+      <img :src="'https://statics.ocs.fr' + film.imageurl">
+      <!--      <a :href="'https://go.ocs.fr/details/' + item.detaillink">detail</a>-->
+      <p>{{film.subtitle}}</p>
+      <p>{{film.detaillink}}</p>
+    </article>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Film",
+  props: {
+    film: {
+      type: Object,
+      required: true
+    },
+    standalone: {
+      // if true, this component process the add to cart otherwise fire an event that another component will read to process the add to cart
+      type: Boolean,
+      default: true
+    }
+  },
+}
+</script>
+
+<style scoped>
+  h2 {
+    font-size: 1.3rem;
+    font-weight: bold;
+  }
+  .film {
+    margin-bottom: 2rem;
+  }
+</style>
---
+++ pages/films.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container is-fullhd">
+    <div v-for="item in result.contents" :key="item.id">
+      <Film :film="item" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Film from "@/components/Film";
+export default {
+  name: "films",
+  components: {Film},
+  async asyncData({ $axios }) {
+    let result = null
+    await $axios
+      .get(`https://api.ocs.fr/web/v2/rubriques/films?sort=new`)
+      .then((res) => {
+        result = res.data
+      })
+
+    return {
+      result
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
